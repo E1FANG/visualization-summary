650c4632b7879a6b844d7967df62be9dfc4e5564
e1fang <438283787@qq.com>
2022-10-08 10:04:16 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+  themeConfig: {
+    siteTitle: '可视化技术总结',
+    nav: [
+      { text: 'Guide', link: '/guide' },
+    ],
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ]
+  }
+}
